f1749e70d28581a13f58e9be0525367ef2c03cf9
Tubby Sparks <adeyemotunbosun20@gmail.com>
2023-09-11 12:31:53 +0100

    finished implementation

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const utc = new Date(
-  new Date().getTime() + newDate().getTimezoneOffset() * 60000
-);
+// const utc = new Date(
+//   new Date().getTime() + newDate().getTimezoneOffset() * 60000
+// );
+let date = new Date();
+let day = date.getDay();
 
 const daysOfWeek = [
   "Sunday",
@@ -15,21 +17,19 @@ const daysOfWeek = [
   "Friday",
   "Saturday",
 ];
-
 const response = app.get("/api", (req, res) => {
+  const { slack_name, track } = req.params;
   res.send({
     slack_name: slack_name,
-    current_day: daysOfWeek[new Date().getDay()],
-    utc_time: utc.toISOString().split(".")[0] + "Z",
-    track: "track",
+    track: track,
+    current_day: daysOfWeek[day],
+    utc_time: date.toISOString().split(".")[0] + "Z",
     github_file_url:
       "https://github.com/TubbySparks/first-zuri-task/blob/main/index.js",
     github_repo_url: "https://github.com/TubbySparks/first-zuri-task",
     status_code: 200,
   });
 });
-
-const { slack_name, track } = req.params;
 
 app.listen(3000, () => {
   console.log(`Example app listening on port ${port}`);
