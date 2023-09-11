040602a53b4bfce653cc5b78fcb1a2d622e7471a
Tubby Sparks <adeyemotunbosun20@gmail.com>
2023-09-11 11:24:18 +0100

    added date issue stuff

--- index.js
+++ index.js
@@ -2,21 +2,34 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get((req, res) => {
+const utc = new Date(
+  new Date().getTime() + newDate().getTimezoneOffset() * 60000
+);
+
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const response = app.get("/api", (req, res) => {
   res.send({
-    slack_name: "TubbySparks",
-    current_day: "Sunday",
-    utc_time: "2023-08-21T15:04:05Z",
-    track: "backend",
-    github_file_url: "",
-    github_repo_url: "",
-    status_code: "200",
+    slack_name: slack_name,
+    current_day: daysOfWeek[new Date().getDay()],
+    utc_time: utc.toISOString().split(".")[0] + "Z",
+    track: "track",
+    github_file_url:
+      "https://github.com/TubbySparks/first-zuri-task/blob/main/index.js",
+    github_repo_url: "https://github.com/TubbySparks/first-zuri-task",
+    status_code: 200,
   });
 });
 
-app.use((req, res) => {
-  console.log("we have a new request");
-});
+const { slack_name, track } = req.params;
 
 app.listen(3000, () => {
   console.log(`Example app listening on port ${port}`);
